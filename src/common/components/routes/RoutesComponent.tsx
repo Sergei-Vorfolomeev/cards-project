bcdffce552b69d8ea8219e87b09dbb0b9e7a805d
Alenka <alenazarubi@gmail.com>
2023-03-04 14:42:15 +0300

    feat(profile)created profile component

--- src/common/components/routes/RoutesComponent.tsx
+++ src/common/components/routes/RoutesComponent.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Login } from '../../../features/login/Login'
+import { Profile } from "../../../features/profile/Profile";
 
 export const RoutesComponent = () => {
   return (
     <Routes>
       {/*add your routes here*/}
       <Route path={'/login'} element={<Login />} />
+      <Route path={'/profile'} element={<Profile />} />
     </Routes>
   )
 }
