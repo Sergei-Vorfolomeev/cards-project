b736cc9dd231f1f05a0cce78af54e813f638b5db
Nadyushka <12nadya11@gmail.com>
2023-03-04 16:05:21 +0300

    feat: create Registartion component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import { Header } from '../features/header/Header'
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/*<Header />*/}
       <RoutesComponent />
     </div>
   )
