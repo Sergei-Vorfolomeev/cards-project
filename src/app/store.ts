5832db75a9d96dbb2bcffdfef935ab7a8559061d
Nadyushka <12nadya11@gmail.com>
2023-03-04 21:17:10 +0300

    feat: add validation to Registration component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,16 @@
-import { AnyAction, combineReducers, legacy_createStore } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
+import thunk, { ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { reducer } from './reducer'
+import {registrationReducer} from "../features/registation/registration-reducer";
 
 // store
 const rootReducer = combineReducers({
   reducer: reducer,
+  registrationReducer: registrationReducer
+
 })
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
 
 //custom hooks
 export const useAppDispatch = () => useDispatch<AppThunkDispatchType>()
