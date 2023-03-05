1db076a7c1ac89d2c63fd2e9773e5b0fcb6a4250
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-03-05 18:05:40 +0100

    feat: login, mark-up in progress

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,58 +1,75 @@
 import React from 'react'
-import { Formik, Field, Form, ErrorMessage } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 import { useAppDispatch, useAppSelector } from '../../app/store'
 import { loginTC } from './authReducer'
-import { Navigate } from 'react-router-dom'
-import { Button } from '../../common/components/button/Button'
+import { NavLink } from 'react-router-dom'
+import styles from './Login.module.css'
+import { Loader } from '../../common/components/loader/Loader'
 
 export const Login = () => {
   const isAuth = useAppSelector<boolean>(state => state.auth.isAuth)
+  const loading = useAppSelector(state => state.registrationReducer.loading)
   const dispatch = useAppDispatch()
 
-  if (isAuth) return <Navigate to={'/profile'} />
+  // if (isAuth) return <Navigate to={'/profile'} />
   return (
-    <Formik
-      initialValues={{ email: '', password: '', rememberMe: false }}
-      validationSchema={Yup.object({
-        email: Yup.string().email('Invalid email address').required('Required'),
-        password: Yup.string()
-          .max(25, 'Password must be 25 characters or less')
-          .min(5, 'Password must be 5 characters or more')
-          .required('Required'),
-      })}
-      onSubmit={(values, { resetForm }) => {
-        dispatch(loginTC(values))
-      }}
-    >
-      <Form>
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <Field name="email" type="email" />
-          <ErrorMessage name="email" />
-        </div>
+    <div className={styles.login}>
+      {loading && <Loader />}
+      <h2>Sign In</h2>
+      <Formik
+        initialValues={{ email: '', password: '', rememberMe: false }}
+        validationSchema={Yup.object({
+          email: Yup.string().email('Invalid email address').required('Required field'),
+          password: Yup.string()
+            .max(25, 'Password must be 25 characters or less')
+            .min(5, 'Password must be 5 characters or more')
+            .required('Required field'),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          dispatch(loginTC(values))
+          resetForm()
+        }}
+      >
+        <Form>
+          <div className={styles.loginInputContainer}>
+            <label htmlFor="email" className={styles.loginInputLabel}>
+              Email
+            </label>
+            <Field name="email" type="email" className={styles.loginInput} />
+            <ErrorMessage name="email" />
+          </div>
 
-        <div>
-          <label htmlFor="password">Password</label>
-          <Field name="password" type="password" />
-          <ErrorMessage name="password" />
-        </div>
+          <div>
+            <label htmlFor="password" className={styles.loginInputLabel}>
+              Password
+            </label>
+            <Field name="password" type="password" className={styles.loginInput} />
+            <ErrorMessage name="password" />
+          </div>
 
-        <div>
-          <label htmlFor="rememberMe">Remember me</label>
-          <Field name="rememberMe" type="checkbox" />
-          <ErrorMessage name="rememberMe" />
-        </div>
+          <div>
+            <Field name="rememberMe" type="checkbox" className={styles.loginCheckbox} />
+            <label htmlFor="rememberMe" className={styles.rememberMeLabel}>
+              Remember me
+            </label>
+            <ErrorMessage name="rememberMe" />
+          </div>
 
-        <div>
-          <span onClick={() => <Navigate to={'/recover-password'} />}>Forgot password?</span>
-        </div>
+          <div className={styles.forgotPasswordContainer}>
+            <NavLink to={'/recover-password'} className={styles.forgotPassword}>
+              Forgot password?
+            </NavLink>
+          </div>
 
-        <button type="submit">Sign In</button>
+          <button type="submit">Sign In</button>
 
-        <div>Already have an account?</div>
-        <Button name={'Sign Up'} callBack={() => <Navigate to={'/registration'} />} />
-      </Form>
-    </Formik>
+          <div>Already have an account?</div>
+          <p>
+            <NavLink to={'/registration'}>Sign Up</NavLink>
+          </p>
+        </Form>
+      </Formik>
+    </div>
   )
 }
