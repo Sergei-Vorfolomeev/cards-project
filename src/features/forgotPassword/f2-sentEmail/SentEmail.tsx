3b16bb9b28b463d998ed51db181ff2c47932d3fe
Nadyushka <12nadya11@gmail.com>
2023-03-08 17:14:36 +0300

    feat: create SentEmailComponent

--- src/features/forgotPassword/f2-sentEmail/SentEmail.tsx
+++ src/features/forgotPassword/f2-sentEmail/SentEmail.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import s from './SentEmail.module.css'
+import ForgotTitle from "../f4-common/forgotTitle/ForgotTitle";
+import checkEmailImg from '../../../common/assets/pictures/checkEmail.svg'
+import ForgotButton from "../f4-common/forgotButton/ForgotButton";
+import {useAppSelector} from "../../../app/store";
+import {useNavigate} from "react-router-dom";
+import { PATH } from '../../../common/components/routes/RoutesComponent';
 
 const SentEmail = () => {
-    return (
-        <div>
 
+    const email = useAppSelector(state=>state.forgotReducer.email)
+    const navigate = useNavigate()
+
+    const buttonOnClickHandler = () => {
+        return navigate(PATH.LOGIN)
+    }
+
+    return (
+        <div className={s.sentEmail}>
+            <div className={s.sentEmail_container}>
+                <ForgotTitle text={'Check Email'}/>
+                <img src={checkEmailImg} alt={'check Email picture'}/>
+                <p>We’ve sent an Email with instructions to </p>
+                <p>{email}</p>
+                <ForgotButton text={'Back to login'} onClick={buttonOnClickHandler}/>
+            </div>
         </div>
     );
 };
